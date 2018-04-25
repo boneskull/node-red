9ed29fb392508059d9727af946a12a7cdef79696
Christopher Hiller <boneskull@boneskull.com>
2018-04-25 14:56:48 -0700

    wallaby: fixes
    Signed-off-by: Christopher Hiller <boneskull@boneskull.com>

--- .wallaby.js
+++ .wallaby.js
@@ -1,27 +1,42 @@
 'use strict';
 
+const path = require('path');
+
+const RUNTIME_PATH = 'packages/runtime';
+const CORE_NODES_PATH = 'packages/core-nodes';;
+
 module.exports = wallaby => {
+  process.env.NODE_PATH += path.delimiter 
+    + path.join(wallaby.localProjectDir, RUNTIME_PATH, 'node_modules') 
+    + path.delimiter 
+    + path.join(wallaby.localProjectDir, CORE_NODES_PATH, 'node_modules');
+
   return {
     files: [
-      'packages/core-nodes/nodes/**/*.js',
-      'packages/core-nodes/nodes/**/*.html',
-      'packages/runtime/api/**/*.js',
-      'packages/runtime/runtime/**/*.js',
-      'packages/core-nodes/test/helper.js'
+      `${CORE_NODES_PATH}/nodes/**/*.js`,
+      {pattern: `${CORE_NODES_PATH}/nodes/**/*.html`, instrument: false},
+      {pattern: `${CORE_NODES_PATH}/test/resources/**`, instrument: false},
+      `${CORE_NODES_PATH}/test/helper.js`,
+      `${RUNTIME_PATH}/api/**/*.js`,
+      `${RUNTIME_PATH}/runtime/**/*.js`,
+      `${RUNTIME_PATH}/red.js`,
+      `${RUNTIME_PATH}/package.json`,
     ],
 
     tests: [
-      'packages/core-nodes/test/**/*_spec.js',
-      'packages/runtime/test/**/*_spec.js'
+      `${CORE_NODES_PATH}/test/**/*_spec.js`,
+      `${RUNTIME_PATH}/test/**/*_spec.js`
     ],
 
     env: {
-      type: 'node'
+      type: 'node',
+      runner: 'node'
     },
 
     setup() {
       wallaby.testFramework.timeout(3000);
     },
+    
     debug:true
   };
 };
